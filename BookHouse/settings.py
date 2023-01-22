bdc19e1fca89e9068d418f08e2ada0e3f3d8e40f
unknown <neeleshbhadoriya786@gmail.com>
2023-01-22 17:33:46 +0530

    static DIR updated

--- BookHouse/settings.py
+++ BookHouse/settings.py
@@ -118,7 +118,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_URL = 'BookHouse/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'BookHouse/static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL= '/media/'
