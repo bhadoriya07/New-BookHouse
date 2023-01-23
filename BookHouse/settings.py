fe6ddcd32cd086efb379f11159fcd364f98f6adf
unknown <neeleshbhadoriya786@gmail.com>
2023-01-23 11:41:52 +0530

    configuration changed

--- BookHouse/settings.py
+++ BookHouse/settings.py
@@ -4,7 +4,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
-# STATIC_DIR=os.path.join(BASE_DIR,'static')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
+
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -118,8 +119,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
 
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
